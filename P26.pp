8b3fc26760961ce1588cccdb2eec7315f6d489c3
Amir Aavani <rima@google.com>
2015-06-23 14:51:50 -0700

    P26

---
+++ P26.pp
@@ -0,0 +1,16 @@
+program P26;
+uses
+  fgl;
+type
+  TIntIntMap = specialize TFPGMap<Integer, Integer>;
+
+function ComputeReciprocalCycleLength(n: Integer): Integer;
+begin
+
+end;
+
+var
+  i, j: Integer;
+
+begin
+end.
